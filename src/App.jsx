10fd77039625ac0444dfcd5ab84a686b22b80ca4
haykal.islam.b <haykal.islam.b@gmail.com>
2024-11-05 10:33:53 +0000

    memulihkan file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import SignUp from "./pages/SignUp";
-import EmailCreate from "./pages/EmailCreate";
-import EmailCode from "./pages/EmailCode";
-import SignIn from "./pages/SignIn";
-import ForgotPassword from "./pages/ForgotPassword";
-import ForgotPasswordCode from "./pages/ForgotPasswordCode";
-import Beranda from "./pages/Beranda"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import SignUp from './pages/SignUp';
+import EmailCreate from './pages/EmailCreate';
+import EmailCode from './pages/EmailCode';
+import SignIn from './pages/SignIn';
+import ForgotPassword from './pages/ForgotPassword';
+import ForgotPasswordCode from './pages/ForgotPasswordCode';
+import Beranda from './pages/Beranda';
+import PersonalSetting from './pages/PersonalSetting';
+import PasswordSetting from './pages/PasswordSetting';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/forgot-password-code" element={<ForgotPasswordCode />} />
       <Route path="/Beranda" element={<Beranda />} />
+      <Route path="/personal-setting" element={<PersonalSetting />} />
+      <Route path="/password-setting" element={<PasswordSetting />} />
     </Routes>
   );
 }
