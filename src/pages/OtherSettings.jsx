4378229c1125d665eef7ca333de2d4891100c2cf
Moch Achmar <iyekodong86@gmail.com>
2024-11-13 22:13:58 +0800

    menambahkan button untuk menghapus riwayat deteksi tanaman di pengaturan bagian lainnya

--- src/pages/OtherSettings.jsx
+++ src/pages/OtherSettings.jsx
@@ -54,12 +54,7 @@ const OtherSettings = () => {
           </select>
         </div>
 
-        <div className="flex items-center mb-4">
-          <label className="text-gray-600 mr-4">Tampilkan keterangan alat</label>
-          <input type="checkbox" className="w-5 h-5 text-green-500 rounded" checked />
-        </div>
-
-        <button className="bg-red-100 text-red-500 rounded-md px-4 py-2 mb-8">Setel ulang default</button>
+        <button className="bg-red-100 text-red-500 rounded-md px-4 py-2 mb-8">Hapus riwayat tanaman</button>
 
         <div className="flex justify-end space-x-4">
           <button className="border border-gray-300 text-gray-700 rounded-md px-4 py-2">Batal</button>
