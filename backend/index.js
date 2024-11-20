0bfaa57ca5f6ddc729fa3faa7656dd71ccb31484
masda_naswa <masdanaswa04@gmail.com>
2024-11-20 22:23:03 +0700

    update articles

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { testConnection } from './config/db.js';
 import articlesRoutes from './routes/articlesRoutes.js';
+import { testConnection } from './config/db.js';
 
-dotenv.config();
+dotenv.config(); // Load .env configuration
 
 const app = express();
 
-app.use(cors());
+// Konfigurasi CORS
+const corsOptions = {
+  origin: 'http://localhost:5173', // Alamat frontend saat pengembangan
+  credentials: true, // Aktifkan jika ada cookies atau session
+};
+app.use(cors(corsOptions));
+
+// Middleware untuk parsing JSON
 app.use(express.json());
 
 // Routes
-app.use('/api', articlesRoutes);
+app.use('/api/articles', articlesRoutes);
 
-// Test DB connection and start server
+// Test koneksi ke database
 testConnection();
 
+// Jalankan server
 const PORT = process.env.SERVER_PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server berjalan di http://localhost:${PORT}`);
 });
--- backend/routes/articlesRoutes.js
+++ backend/routes/articlesRoutes.js
@@ -1,24 +1,14 @@
 import express from 'express';
-import multer from 'multer';
 import articlesController from '../controller/articlesController.js';
+import { singleUpload } from '../middleware/uploadMiddleware.js';
 
 const router = express.Router();
 
-// Konfigurasi multer untuk penyimpanan file
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Folder penyimpanan
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname); // Nama file unik
-  },
-});
-const upload = multer({ storage });
-
-router.get('/articles', articlesController.getAllArticles);
-router.get('/articles/:id', articlesController.getArticleById);
-router.post('/articles', upload.single('thumbnail_image'), articlesController.createArticle);
-router.put('/articles/:id', upload.single('thumbnail_image'), articlesController.updateArticle);
-router.delete('/articles/:id', articlesController.deleteArticle);
+// API untuk articles
+router.get('/', articlesController.getAllArticles);
+router.get('/:id', articlesController.getArticleById);
+router.post('/', singleUpload, articlesController.createArticle);
+router.put('/:id', singleUpload, articlesController.updateArticle);
+router.delete('/:id', articlesController.deleteArticle);
 
 export default router;
