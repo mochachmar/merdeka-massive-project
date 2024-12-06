56692e21287af78303e8a0a09679993d1fb9b4ee
Moch Achmar <iyekodong86@gmail.com>
2024-12-07 01:28:13 +0800

    Menghapus file main

--- backend/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addPlantHistory } from '../controllers/plantController.js';
-import upload from '../middleware/uploadTanamanMiddleware.js';
-const router = express.Router();
-
-// Rute untuk menambahkan tanaman
-router.post('/upload-tanaman',  upload.single('file'), addPlantHistory);
-
-export default router;
