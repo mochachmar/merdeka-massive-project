e73b22accd9e70816b3f83ec62ef1dc5ee0aac43
Moch Achmar <iyekodong86@gmail.com>
2024-12-07 15:03:40 +0800

    Memperbaiki tampilan card artikel pada beranda untuk device tablets/ipad

--- frontend/src/pages/Article.jsx
+++ frontend/src/pages/Article.jsx
@@ -1,19 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Footer from '../components/Footer.jsx';
-import Card from '../components/Card.jsx';
 import { Link, useParams } from 'react-router-dom';
 import arrowBack from '../assets/arrow-back.svg';
 import Author from '../assets/Author.svg';
-import gambarSelada from '../assets/selada.png';
-import gambarBayam from '../assets/bayam.png';
-import gambarKale from '../assets/kale.png';
-import gambarArigula from '../assets/arigula.png';
-import gambarPakcoi from '../assets/pakcoi.png';
-import gambarArtikel from '../assets/tomat.png';
-import gambarCabai from '../assets/cabai.png';
-import gambarTimun from '../assets/timun.png';
-import gambarPaprika from '../assets/paprika.png';
 import axios from 'axios';
 
 // Artikel saat dibuka
@@ -105,37 +95,37 @@ function Article(props) {
 }
 
 function CardDummy({ article }) {
- // Format tanggal ke dalam bahasa Indonesia
- const formattedDate = new Date(article.publish_date).toLocaleDateString('id-ID', {
-  weekday: 'long',
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-});
+  // Format tanggal ke dalam bahasa Indonesia
+  const formattedDate = new Date(article.publish_date).toLocaleDateString('id-ID', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-return (
-  <Link
-    className="w-[346px] rounded-lg overflow-hidden shadow-lg cursor-pointer transform transition-transform hover:scale-105 duration-300"
-    key={article.id}
-    to={`/article/${article.article_id}`}
-    style={{
-      backgroundImage: `url(http://localhost:3000/uploads/${article.thumbnail_image})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}
-  >
-    {/* Empty Space */}
-    <div className="h-40"></div>
+  return (
+    <Link
+      className="w-[346px] rounded-lg overflow-hidden shadow-lg cursor-pointer transform transition-transform hover:scale-105 duration-300"
+      key={article.id}
+      to={`/article/${article.article_id}`}
+      style={{
+        backgroundImage: `url(http://localhost:3000/uploads/${article.thumbnail_image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      {/* Empty Space */}
+      <div className="h-40"></div>
 
-    {/* Text Overlay Container with Backdrop Opacity */}
-    <div className="bg-[#C4C8AD] bg-opacity-70 p-6 rounded-lg">
-      <p className="text-gray-900 text-sm">{formattedDate || 'Tanggal tidak tersedia'}</p>
-      <h2 className="font-bold text-lg text-gray-950">{article.title}</h2>
-      <p className="text-gray-950 text-sm">{article.short_description}</p>
-      {article.author && <p className="text-gray-800 text-sm mt-1">By {article.author}</p>}
-    </div>
-  </Link>
-);
+      {/* Text Overlay Container with Backdrop Opacity */}
+      <div className="bg-[#C4C8AD] bg-opacity-70 p-6 rounded-lg">
+        <p className="text-gray-900 text-sm">{formattedDate || 'Tanggal tidak tersedia'}</p>
+        <h2 className="font-bold text-lg text-gray-950">{article.title}</h2>
+        <p className="text-gray-950 text-sm">{article.short_description}</p>
+        {article.author && <p className="text-gray-800 text-sm mt-1">By {article.author}</p>}
+      </div>
+    </Link>
+  );
 }
 
 export default Article;
